371146e2a2494fefae2e1bee0fa1f74fda1d102a
Pedro Henrique Brasil <pedrohrfbrasil@gmail.com>
2023-04-04 22:16:48 -0300

    G.sql

---
+++ G.sql
@@ -0,0 +1,15 @@
+# CADASTRAR CARTEGORIA DE PRODUTOS
+
+INSERT INTO mc_categoria_prod (cd_categoria, tp_categoria, ds_categoria, st_categoria) VALUES (sq_mc_categoria.nextval, 'P', 'Artesanato', 'A');
+INSERT INTO mc_categoria_prod (cd_categoria, tp_categoria, ds_categoria, st_categoria) VALUES (sq_mc_categoria.nextval, 'V', 'Áudio', 'A');
+INSERT INTO mc_categoria_prod (cd_categoria, tp_categoria, ds_categoria, st_categoria) VALUES (sq_mc_categoria.nextval, 'P', 'Brinquedos', 'A');
+INSERT INTO mc_categoria_prod (cd_categoria, tp_categoria, ds_categoria, st_categoria) VALUES (sq_mc_categoria.nextval, 'P', 'Celular e Smartphone', 'A');
+INSERT INTO mc_categoria_prod (cd_categoria, tp_categoria, ds_categoria, st_categoria) VALUES (sq_mc_categoria.nextval, 'P', 'Colchão', 'A');
+INSERT INTO mc_categoria_prod (cd_categoria, tp_categoria, ds_categoria, st_categoria) VALUES (sq_mc_categoria.nextval, 'P', 'Esporte e Lazer', 'A');
+INSERT INTO mc_categoria_prod (cd_categoria, tp_categoria, ds_categoria, st_categoria) VALUES (sq_mc_categoria.nextval, 'P', 'Ferramentas', 'A');
+INSERT INTO mc_categoria_prod (cd_categoria, tp_categoria, ds_categoria, st_categoria) VALUES (sq_mc_categoria.nextval, 'V', 'Games', 'A');
+INSERT INTO mc_categoria_prod (cd_categoria, tp_categoria, ds_categoria, st_categoria) VALUES (sq_mc_categoria.nextval, 'V', 'Informática', 'A');
+INSERT INTO mc_categoria_prod (cd_categoria, tp_categoria, ds_categoria, st_categoria) VALUES (sq_mc_categoria.nextval, 'P', 'Livros', 'A');
+INSERT INTO mc_categoria_prod (cd_categoria, tp_categoria, ds_categoria, st_categoria) VALUES (sq_mc_categoria.nextval, 'P', 'Pet Shop', 'A');
+INSERT INTO mc_categoria_prod (cd_categoria, tp_categoria, ds_categoria, st_categoria) VALUES (sq_mc_categoria.nextval, 'V', 'TV', 'A');
+INSERT INTO mc_categoria_prod (cd_categoria, tp_categoria, ds_categoria, st_categoria) VALUES (sq_mc_categoria.nextval, 'V', 'Utilidades Domésticas', 'A');
